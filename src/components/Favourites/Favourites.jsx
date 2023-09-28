eafa5cfef4c5a893e474cd3ac0e2183d9f1849b6
omarfaruk70 <faruk1611281892@gmail.com>
2023-09-28 13:38:52 +0600

    dynamically render show more and show less button with slice method

--- src/components/Favourites/Favourites.jsx
+++ src/components/Favourites/Favourites.jsx
@@ -3,7 +3,8 @@ import FavouriteCard from "../FavouriteCard/FavouriteCard";
 
 const Favourites = () => {
     const [favourteitem, setFavouriteitem] = useState([]);
-    const [noFound, setNoFound] = useState(false)
+    const [noFound, setNoFound] = useState(false);
+    const [isShow, setIsShow] = useState(false)
     useEffect(()=>{
         const getDataFromLocalStorage = JSON.parse(localStorage.getItem('favouritesPhone'));
       if(getDataFromLocalStorage){
@@ -11,18 +12,36 @@ const Favourites = () => {
       }else{
         setNoFound('No Favourite items Found')
       }
-
     },[])
-    console.log(favourteitem);
+    // console.log(favourteitem);
+
+    const handleRemoveAll = () =>{
+        localStorage.clear();
+        setFavouriteitem([]);
+        setNoFound('No Favourite items Found')
+    }
     return (
         <div>
           {noFound ? <p className="text-2xl flex h-screen items-center justify-center">{noFound}</p>
         :
         <div>
+            <div className="py-2 flex justify-center items-center">
+                {
+                favourteitem.length > 0 && <button onClick={handleRemoveAll} className="py-3 px-3 bg-blue-400 text-white rounded-lg">Clear All</button>
+                }
+            </div>
             <div className="grid grid-cols-2 mx-auto py-10">
                 {
-                    favourteitem.map(singleItem => <FavouriteCard key={singleItem.id} singleItem={singleItem}></FavouriteCard>)
+                    isShow ? favourteitem.map(singleItem => <FavouriteCard key={singleItem.id} singleItem={singleItem}></FavouriteCard>) 
+                    :
+                    favourteitem.slice(0, 4).map(singleItem => <FavouriteCard key={singleItem.id} singleItem={singleItem}></FavouriteCard>)
                 }
+            </div>
+
+            <div className="py-2 flex justify-center items-center">
+             <button onClick={()=> setIsShow(!isShow)} className="py-3 px-3 bg-blue-400 text-white rounded-lg">
+                { isShow? 'See Less' : 'See More'}
+                </button>
             </div>
             
         </div>}
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -73,7 +73,7 @@ const Login = () => {
           Sign In
         </button>
         <p className="mt-6 flex justify-center font-sans text-sm font-light leading-normal text-inherit antialiased">
-          Don't have an account?
+          Do not have an account?
           <a
             href="#signup"
             className="ml-1 block font-sans text-sm font-bold leading-normal text-pink-500 antialiased"
